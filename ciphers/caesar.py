3b3601213e5afc67c8f985db8065e8615db68eef
Dominik Biscan <dominik.biscan444@gmail.com>
2026-09-03 15:26:03 +0200

    Add Caesar cipher and project documentation

---
+++ ciphers/caesar.py
@@ -0,0 +1,96 @@
+alphabet_uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+alphabet_lowercase = [letter.lower() for letter in alphabet_uppercase]
+
+#Function used to print information about the cypher
+def info():
+    print()
+    print("The Caesar cipher is a simple way to encrypt messages.")
+    print("It shifts each letter a set number of places in the alphabet.")
+    print("It is named after Julius Caesar, who used it to encode messages in his correspondence.")
+
+#Function that acquires the amount of positions that the letters will shift
+def get_shift():
+    while True:
+        try:
+            shift = int(input("Enter shift amount: "))      #How many positions to shift
+            break
+        except ValueError:
+            print()
+            print("Please enter a number.")
+
+    return shift
+
+#Letter encryption/decryption function
+def crypt_letter(character, alphabet, shift):
+    position = alphabet.index(character)
+    new_position = (position + shift) % 26
+
+    return alphabet[new_position]
+
+#Text encryption/decryption function --- uses crypt_letter function
+def crypt_text(text, shift):
+    result = ""
+
+    character_list = list(text)     #Divide original text into a list of characters
+
+    for character in character_list:
+    #Handles Upper Case Letters
+        if character in alphabet_uppercase:
+            result = result + crypt_letter(character, alphabet_uppercase, shift)
+
+        #Handles Lower Case Letters
+        elif character in alphabet_lowercase:
+            result = result + crypt_letter(character, alphabet_lowercase, shift)
+
+        #Handles everything else that is not a letter
+        else:
+            result = result + character
+
+    return result
+
+#Function that formats the result
+def display_result(original, result, shift, operation):
+    print()
+    print("Original message:", original)
+    print(operation + " message:", result)
+    print("Shift:", shift)
+
+#=======================================================================================#
+
+while True:
+    print()
+    print("=== Caesar Cipher ===")
+    print("1) Encrypt")
+    print("2) Decrypt")
+    print("I) Info")
+    print("E) Exit") 
+
+    choice = input("Enter your choice: ")
+    choice = choice.lower()
+
+    if choice == "1":
+        text = input("Enter text you would like to encrypt: ")
+        shift = get_shift()
+        
+        result = crypt_text(text, shift)
+
+        display_result(text, result, shift, "Encrypted")
+
+    elif choice == "2":
+        text = input("Enter encrypted text you would like to decrypt: ")
+        shift = get_shift()
+
+        result = crypt_text(text, -shift)
+
+        display_result(text, result, shift, "Decrypted")
+
+    elif choice == "i":
+        info()
+        print()
+        input("Press Enter to continue...")
+
+    elif choice == "e":
+        break
+
+    else:
+        print("Invalid choice. Try again.")
---
+++ documentation/PROJECT_LOG.md
@@ -0,0 +1,17 @@
+# Project Log
+
+## 2026-09-03
+
+### Starting point
+
+This project currently consists of a basic Python menu program.
+
+### Goal
+
+I want to develop this into an educational cipher tool where users can select different cipher algorithms and encrypt or decrypt text.
+
+### Goal for today
+
+Today I plan to develop a Caesar cipher Python script where the user can choose to enter text to encrypt or enter already encrypted text and decrypt it.
+
+Perhaps later I will also experiment with having `main.py` communicate with a separate Python file.
